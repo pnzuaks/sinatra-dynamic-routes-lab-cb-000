25770efb52d86381728562d58e799700f0504c08
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-10 19:20:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/square/:num" do
+  get "/square/:number" do
     @num = params[:num]
     "#{@num.to_i * @num.to_i}"
   end
 
-  get "/:opsquare/:num" do
+  get "'/:operation/:number1/:number2'" do
     @num = params[:num]
     "#{@num.to_i * @num.to_i}"
   end
