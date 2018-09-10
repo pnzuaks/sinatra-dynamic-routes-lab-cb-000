57b83491c0d2182f36fdbbc2e7c8d805fca140d9
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-10 19:19:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/multiply/:num1/:num2" do
-    @num_1 = params[:num1]
-    @num_2 = params[:num2]
-    # erb :multiply
-    # @num_1*@num_2
-    "#{@num_1.to_i * @num_2.to_i}"
+  get "/square/:num" do
+    @num = params[:num]
+    "#{@num.to_i * @num.to_i}"
 
   end
 
