6cda157de6b92535e46f781df3f175172b984302
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-10 19:27:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,10 @@ class App < Sinatra::Base
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
-    @word4 = params[:word1]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+
   end
 
   get "/square/:number" do
